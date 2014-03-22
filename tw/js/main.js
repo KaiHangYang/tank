dd0d5b75efcf765ee44b6d99d63bbd872be9f3a0
as <y907987244@gmail.com>
2014-03-23 00:07:32 +0800

    tank

--- tw/js/main.js
+++ tw/js/main.js
@@ -1,173 +1,116 @@
-window.onload = function(){
-	//这里是制造地图的矩阵 一小格代表20px；
-	var mapCanv = document.getElementById("canv1");
-	var mapCtx = mapCanv.getContext("2d");
-	var warCanv = document.getElementById("canv2");
-	var warCtx = warCanv.getContext("2d");
-
-	var mapVar = [
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,7,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,8,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,1,1,9,0,1,1,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0]
-		];
-	//下来是对图像进行加载。
-	var wall = new Image();
-	var steel = new Image();
-	var grass = new Image();
-	var water = new Image();
-	var buff1 = new Image();
-	var buff2 = new Image();
-	var buff3 = new Image();
-	var buff4 = new Image();
-	var base = new Image();
-	var player1 = new Array(4);
-	var player2 = new Array(4);
-	for (var i=0; i <= 3; i++){
-		player1[i] = new Image();
-	}
-	for (var i=0; i <= 3; i++){
-		player2[i] = new Image();
-	}
-	var pbul = new Image();
-	var ebul = new Image();
-
-	wall.src = "media/img/wall/wall.gif";
-	steel.src = "media/img/wall/steel.gif";
-	grass.src = "media/img/wall/grass.gif";
-	water.src = "media/img/wall/water.gif";
-	buff1.src = "media/img/buff/bomb.gif";
-	buff2.src = "media/img/buff/mintank.gif";
-	buff3.src = "media/img/buff/star.gif";
-	buff4.src = "media/img/buff/timer.gif";
-	base.src = "media/img/others/symbol.gif";
-	player1[0].src = "media/img/player/p1tankU.gif";
-	player1[1].src = "media/img/player/p1tankR.gif";
-	player1[2].src = "media/img/player/p1tankD.gif";
-	player1[3].src = "media/img/player/p1tankL.gif";
-	player2[0].src = "media/img/player/p2tankU.gif";
-	player2[1].src = "media/img/player/p2tankR.gif";
-	player2[2].src = "media/img/player/p2tankD.gif";
-	player2[3].src = "media/img/player/p2tankL.gif";
-	pbul.src = "media/img/others/playerbullet.gif";
-	ebul.src = "media/img/others/enemybullet.gif";
-	//下来是地图对象map话说只用画墙还有增益
-	function norMap(){
-		var map = {};
-		map.wallP = [{X:0,Y:0}];//墙代表1
-		map.waterP = [{X:2,Y:0}];//水代表3
-		map.steelP = [{X:4,Y:0}];//金属代表2
-		map.grassP = [{X:6,Y:0}];//草代表4
-		map.buff = [{X:0,Y:0},{X:0,Y:0},{X:0,Y:0},{X:0,Y:0}];//buff[0] 5代表bomb，buff[1] 6mintank，buff[2] 7star，buff[3] 8timer。
-
-		map.draw = function(){
-			//先画墙再画buff
-			for (i in mapVar){
-				for (j in mapVar[i]){
-					switch(mapVar[i][j]){
-						case 1:mapCtx.drawImage(wall,j*20,i*20,20,20);break;
-						case 2:mapCtx.drawImage(steel,j*20,i*20,20,20);break;
-						case 3:mapCtx.drawImage(water,j*20,i*20,20,20);break;
-						case 4:mapCtx.drawImage(grass,j*20,i*20,20,20);break;
-						case 9:mapCtx.drawImage(base,j*20,i*20,40,40);break;
-						default:break;
-					}
-				}
-			}
-			for (i in mapVar){
-				for (j in mapVar[i]){
-					switch(mapVar[i][j]){
-						case 5: mapCtx.drawImage(buff1,j*20,i*20,20,20);break;
-						case 6: mapCtx.drawImage(buff2,j*20,i*20,20,20);break;
-						case 7: mapCtx.drawImage(buff3,j*20,i*20,20,20);break;
-						case 8: mapCtx.drawImage(buff4,j*20,i*20,20,20);break;
-						default:break;
-					}
-				}
-			}
+//先搞出canvas’画笔‘
+canv = document.getElementById("canv1").getContext('2d');
+//先写出来所需要的数组
+enemyBullet = new Array(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+player1Bullet = null;
+//写tank出生的类
+function tankBorn(x, y){
+	var tBorn = {};
+	tBorn.x = x;
+	tBorn.y = y;
+	tBorn.time = 0;
+	tBorn.born = true;
+	
+	tBorn.drawBorn = function drawBorn(){
+		if (tBorn.time <= 1){
+			canv.drawImage(born[0], tBorn.x, tBorn.y, 40, 40);
 		}
-		return map;
-	}
-	function warMap(){
-		var map = {};
-		map.draw = function(){
-			warCtx.clearRect(0,0,520,520);
-			warCtx.fillStyle = "rgb(3,3,3)";
-			warCtx.fillRect(0,0,520,520);
+		else if (tBorn.time <= 2){
+			canv.drawImage(born[1], tBorn.x, tBorn.y, 40, 40);
 		}
-		return map;
+		else if (tBorn.time <= 3){
+			canv.drawImage(born[2], tBorn.x, tBorn.y, 40, 40);
+		}
+		else if (tBorn.time <= 4){
+			canv.drawImage(born[3], tBorn.x, tBorn.y, 40, 40);
+		}
+		tBorn.time ++;
+		if (tBorn.time > 4){
+			tBorn.born = false;
+			tBorn.time = 0;
+		}
 	}
-//下面是坦克对象 
-	var U=119,R=100,D=115,L=97;
-
-	sudo apt-get install cinnamon
-	function Tank(){
-		var tank = {};
-
-		tank.pos = {X:100,Y:100};
+	return tBorn;
+}
+//写Tank类
+function Tank(x, y, dir, rate, tankType, isLive){
+	var tank = {};
+	tank.x = x;
+	tank.y = y;
+	tank.dir = dir;
+	tank.rate = rate;
+	tank.tankType = tankType;
+	tank.isLive = isLive;
+	
+	tank.moveUp = function(){
+		tank.y -= tank.rate;
+		if (tank.y <= 0){
+			tank.y = 0;
+		}
 		tank.dir = 0;
+	}
+	tank.moveDown = function(){
+		tank.y += tank.rate;
+		if (tank.y >= 520){
+			tank.y = 520;
+		}
+		tank.dir = 2;
+	}
+	tank.moveRight = function(){
+		tank.x += tank.rate;
+		if (tank.x >= 520){
+			tank.x = 520;
+		}
+		tank.dir = 1;
+	}
+	tank.moveLeft = function(){
+		tank.x -= tank.rate;
+		if (tank.x <= 0){
+			tank.x = 0;
+		}
+		tank.dir = 3;
+	}
+	return tank;
+}
+//然后是子弹类
+function Bullet(x, y, rate, dir){
+	var bullet = {};
+	bullet.x = x;
+	bullet.y = y;
+	bullet.rate = rate;
+	bullet.dir = dir;
+	bullet.time = null;
+	bullet.isLive = true;
 
-		tank.move = function(e){
-			switch(e){
-				case U:tank.dir=0;tank.pos.Y-=10;tank.draw();break;
-				case R:tank.dir=1;tank.pos.X+=10;tank.draw();break;
-				case D:tank.dir=2;tank.pos.Y+=10;tank.draw();break;
-				case L:tank.dir=3;tank.pos.X-=10;tank.draw();break;
+	bullet.run = function(whichTank){
+		if ((bullet.x >= 520 || bullet.x <= 0 || bullet.y >= 520 || bullet.y <= 0) && bullet.isLive){
+			window.clearInterval(bullet.time);
+			bullet.isLive = false;
+			if (whichTank == 'p1'){
+				player1Bullet = null;
+				switch (bullet.dir){
+					case 0: 
+						
+				}
 			}
 		}
-		tank.fire = function(){
-		}
-		tank.draw = function(){
-			b.draw();
-			switch(tank.dir){
-				case 0:warCtx.drawImage(player1[0], tank.pos.X, tank.pos.Y,40,40);break;
-				case 1:warCtx.drawImage(player1[1], tank.pos.X, tank.pos.Y,40,40);break;
-				case 2:warCtx.drawImage(player1[2], tank.pos.X, tank.pos.Y,40,40);break;
-				case 3:warCtx.drawImage(player1[3], tank.pos.X, tank.pos.Y,40,40);break;	
-			}
-		}
-		return tank;
 	}
-	function Bullet(owner,side){
-		var bul = {};
-		bul.pos = {X:0,Y:0};
-		bul.dir = 0;
 
-		bul.draw = function(){
-			
+	return bullet;
+}
+//下来是对于玩家和enemy的tank 需要继承于原来的Tank
+function Player (x, y, dir, rate, tankType, isLive){
+	var player = {};
+	player.tank = new Tank(x, y, dir, rate, tankType, isLive);
+
+	player.attackEnemy = function(){
+		if (playerBullet != null){
+			return;
+		}
+		switch (player.tank.dir){
+			case 0:
+				playerBullet = new
 		}
 	}
-	//下面是实验
-	var a = new norMap();
-	var b = new warMap();
-	var t = new Tank();
-	pbul.onload = function(){a.draw();}
-	b.draw();//上面是实验
-	window.onkeypress = function(event){
-		var e = event.keyCode;
-		t.move(e);
-	}
-	//下面是tank对象
+	return player;
 }
---
+++ tw/js/mediaLoad.js
@@ -0,0 +1,103 @@
+	//上面是图片加载
+	wall = new Image();
+	steel = new Image();
+	grass = new Image();
+	water = new Image();
+	buff1 = new Image();
+	buff2 = new Image();
+	buff3 = new Image();
+	buff4 = new Image();
+ 	base = new Image();
+ 	baseDes = new Image();
+  	player1 = new Array();
+	player2 = new Array();
+	enemy1 = new Array();
+	enemy2 = new Array();
+	enemy3 = new Array();
+	enemy4 = new Array();
+	blast = new Array();
+	born = new Array();
+	//这里是音频加载
+	audStart = new Audio("media/sound/start.wav");
+	audBlast = new Audio("media/sound/blast.wav");
+	audAdd = new Audio("media/sound/add.wav");
+	audFire = new Audio("media/sound/fire.wav");
+	audHit = new Audio("media/sound/hit.wav");
+
+	
+	for (var i=0; i <= 3; i++){
+		player1[i] = new Image();
+	}
+	for (var i=0; i <= 3; i++){
+		player2[i] = new Image();
+	}
+	for (var i=0; i <= 3; i++){
+		enemy1[i] = new Image();
+	}
+	for (var i=0; i <= 3; i++){
+		enemy2[i] = new Image();
+	} 
+	for (var i=0; i <= 3; i++){
+		enemy3[i] = new Image();
+	}
+	for (var i=0; i <= 3; i++){
+		enemy4[i] = new Array();
+	}
+	for (var i=0; i < 3; i++){
+		for (var j=0; j <= 3; j++){
+			enemy4[i][j] = new Image();
+		}
+	}
+	for (var i=0; i<8; i++){
+		blast[i] = new Image();
+	}
+	for (var i=0; i<4; i++){
+		born[i] = new Image();
+	}
+	pbul = new Image();
+	ebul = new Image();
+
+	wall.src = "media/img/wall/wall.gif";
+	steel.src = "media/img/wall/steel.gif";
+	grass.src = "media/img/wall/grass.gif";
+	water.src = "media/img/wall/water.gif";
+	buff1.src = "media/img/buff/bomb.gif";
+	buff2.src = "media/img/buff/mintank.gif";
+	buff3.src = "media/img/buff/star.gif";
+	buff4.src = "media/img/buff/timer.gif";
+	base.src = "media/img/others/symbol.gif";
+	baseDes.src = "media/img/effect/destory.gif";
+	player1[0].src = "media/img/player/p1tankU.gif";
+	player1[1].src = "media/img/player/p1tankR.gif";
+	player1[2].src = "media/img/player/p1tankD.gif";
+	player1[3].src = "media/img/player/p1tankL.gif";
+	player2[0].src = "media/img/player/p2tankU.gif";
+	player2[1].src = "media/img/player/p2tankR.gif";
+	player2[2].src = "media/img/player/p2tankD.gif";
+	player2[3].src = "media/img/player/p2tankL.gif";
+	putImgSrc(1,4,enemy1,"e1");
+	putImgSrc(1,4,enemy2,"e2");
+	putImgSrc(1,4,enemy3,"e3");
+	putImgSrc(3,4,enemy4,"e4");
+	for (i=0; i<8; i++){
+		blast[i].src = "media/img/effect/blast" + (i+1) + ".gif";
+	}
+	for (i=0; i<4; i++){
+		born[i].src = "media/img/effect/born" + (i+1) + ".gif";
+	}
+	pbul.src = "media/img/others/playerbullet.gif";
+	ebul.src = "media/img/others/enemybullet.gif";
+	function putImgSrc(arrD1,arrD2,arrName,srcName){
+		if (arrD1 > 1){
+			for (var i=1; i <= arrD1; i++){
+				for (var j=0; j < arrD2; j++){
+					arrName[i-1][j].src = "media/img/enemy/" + srcName + "_" + i + "_" + j + ".gif";
+				}
+			}
+		}
+		else{
+			for (var i=0; i<arrD2; i++){
+				arrName[i].src = "media/img/enemy/" + srcName + "_" + i + ".gif";
+			}
+		}
+	}
