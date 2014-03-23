a397b7df3af394c646c67d4421b49f7aee831efe
as <y907987244@gmail.com>
2014-03-23 10:41:55 +0800

    newest

--- tw/js/main.js
+++ tw/js/main.js
@@ -1,8 +1,11 @@
-//先搞出canvas’画笔‘
-canv = document.getElementById("canv1").getContext('2d');
+window.onload = function(){
+//画笔
+canv = document.getElementById("canv1").getContext("2d");
 //先写出来所需要的数组
 enemyBullet = new Array(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+enemyBulletBomb = new Array(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
 player1Bullet = null;
+player1BulletBomb = null;
 //写tank出生的类
 function tankBorn(x, y){
 	var tBorn = {};
@@ -32,6 +35,65 @@ function tankBorn(x, y){
 	}
 	return tBorn;
 }
+//然后是子弹爆炸类
+function bulletBomb(x, y){
+	var bBomb = {};
+	bBomb.x = x;
+	bBomb.y = y;
+	bBomb.time = 0;
+	bBomb.isLive = true;
+
+	bBomb.drawBomb = function(){
+		if (bBomb.time <= 1){
+			canv.drawImage(blast[0], bBomb.x, bBomb.y, 20, 20);
+		}
+		else if (bBomb.time <= 2){
+			canv.drawImage(blast[1], bBomb.x, bBomb.y, 20, 20);
+		}
+		else if (bBomb.time <= 3){
+			canv.drawImage(blast[2], bBomb.x, bBomb.y, 20, 20);
+		}
+		else if (bBomb.time <= 4){
+			canv.drawImage(blast[3], bBomb.x, bBomb.y, 20, 20);
+		}
+		bBomb.time ++;
+		if (bBomb.time > 4){
+			bBomb.isLive = false;
+			bBomb.time = 0;
+		}
+	}
+	return bBomb;
+}
+//下来是坦克爆炸类
+function tankBomb(x, y){
+	var tBomb = {};
+	tBomb.x = x;
+	tBomb.y = y;
+	tBomb.time = 0;
+	tBomb.isLive = true;
+	
+	tBomb.drawBomb = function(){
+		if (bBomb.time <= 1){
+			canv.drawImage(blast[4], bBomb.x, bBomb.y, 40, 40);
+		}
+		else if (bBomb.time <= 2){
+			canv.drawImage(blast[5], bBomb.x, bBomb.y, 40, 40);
+		}
+		else if (bBomb.time <= 3){
+			canv.drawImage(blast[6], bBomb.x, bBomb.y, 40, 40);
+		}
+		else if (bBomb.time <= 4){
+			canv.drawImage(blast[7], bBomb.x, bBomb.y, 40, 40);
+		}
+		bBomb.time ++;
+		if (bBomb.time > 4){
+			bBomb.isLive = false;
+			bBomb.time = 0;
+		}
+	}
+	return tBomb;
+}
+
 //写Tank类
 function Tank(x, y, dir, rate, tankType, isLive){
 	var tank = {};
@@ -90,27 +152,97 @@ function Bullet(x, y, rate, dir){
 				player1Bullet = null;
 				switch (bullet.dir){
 					case 0: 
-						
+						player1BulletBomb = new bulletBomb(bullet.x - 5, bullet.y);
+						break;
+					case 1:
+						player1BulletBomb = new bulletBomb(bullet.x - 5, bullet.y - 5);
+						break;
+					case 2:
+						player1BulletBomb = new bulletBomb(bullet.x - 5, bullet.y - 20);
+						break;
+					case 3:
+						player1BulletBomb = new bulletBomb(bullet.x, bullet.y - 5);
+						break;
 				}
+			}
+			else {
+				enemyBulletbomb[whichTank] = null;
+				switch(bullet.dir){
+					case 0:
+						enemyBulletBomb[whichTank] =  new bulletBomb(bullet.x - 5, bullet.y);
+						break;
+					case 1:
+						enemyBulletBomb[whichTank] = new bulletBomb(bullet.x - 5, bullet.y - 5);
+						break;
+					case 2:
+						enemyBulletBomb[whichTank] =  new bulletBomb(bullet.x - 5, bullet.y - 20);
+						break;	
+					case 3:
+						enemyBulletBomb[whichTank] =  new bulletBomb(bullet.x, bullet.y - 5);
+						break;	
+				}
+
+			}
+		}
+		else {
+			switch (bullet.dir){
+				case 0:
+					bullet.y -= bullet.rate;
+					break;
+				case 1:
+					bullet.x += bullet.rate;
+					break;
+				case 2:
+					bullet.y += bullet.rate;
+					break;
+				case 3:
+					bullet.x -= bullet.rate;
+					break;
 			}
 		}
 	}
 
 	return bullet;
 }
-//下来是对于玩家和enemy的tank 需要继承于原来的Tank
+//下来是对于player和enemy的tank 需要继承于原来的Tank
 function Player (x, y, dir, rate, tankType, isLive){
 	var player = {};
-	player.tank = new Tank(x, y, dir, rate, tankType, isLive);
+	player.tank = Tank;
+	player.tank(x, y, dir, rate, tankType, isLive);
 
-	player.attackEnemy = function(){
-		if (playerBullet != null){
+	player.fire = function(){
+		if (player1Bullet != null){
 			return;
 		}
-		switch (player.tank.dir){
+		switch (player.dir){
 			case 0:
-				playerBullet = new
+				player1Bullet = new Bullet(player.x + 15, player.y - 5);
+				break;
+			case 1:
+				player1Bullet = new Bullet(player.x + 40, player.y + 15);
+				break;
+			case 2:
+				player1Bullet = new Bullet(player.x + 15, player.y + 40);
+				break;
+			case 3:
+				player1Bullet = new Bullet(player.x - 5, player.y + 15);
+				break;
 		}
+		player1Bullet.time = window.setInterval("player1Bullet.run('p1')", 20);
 	}
+
 	return player;
+}
+//敌人的坦克类。
+function Enemy(x, y, dir, rate, tankType, isLive){
+	var enemy = {};
+	enemy.tank = Tank;
+	enemy.tank(x, y, dir, rate, tankType, isLive);
+
+	enemy.move = function(){
+	}
+	return enemy;
+}
+
+
 }
